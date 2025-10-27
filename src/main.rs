cd26c5c50611b6ca7b1b165f23706bacdcb41c1c
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-10-27 05:53:56 +0500

    initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -18,7 +19,6 @@ fn main() {
         Err(err) => panic!("{}", err),
     };
     let reader = BufReader::new(file);
-
     let mut lines = reader.lines();
 
     let head = match lines.next() {
@@ -32,14 +32,38 @@ fn main() {
     };
 
     let columns_iter = column_string.trim().split(",");
-
     let columns: Vec<String> = columns_iter.map(|s| s.to_string()).collect();
+
+    let search_index = match columns.iter().position(|i| i == &"UNITPRICE") {
+        Some(data) => data,
+        None => panic!("UNITPRICE header not found"),
+    };
 
     if columns.is_empty() {
         panic!("{}", "CSV file must contain headers")
     }
 
-    println!("{:?}", columns);
+    let mut counter: HashMap<String, i128> = HashMap::new();
 
+    for line in lines {
+        let cells_string = match line {
+            Ok(data) => data,
+            Err(_err) => continue,
+        };
+        let cells_iter = cells_string.trim().split(",");
+        let cells: Vec<String> = cells_iter.map(|i| i.to_string()).collect();
+
+        let cell_value = match cells.get(search_index) {
+            Some(data) => data,
+            None => continue,
+        };
+
+        match counter.get(cell_value) {
+            Some(data) => counter.insert(cell_value.to_string(), data + 1),
+            None => counter.insert(cell_value.to_string(), 0),
+        };
+    }
+
+    println!("{:?}", counter);
     println!("{}", args.function);
 }
